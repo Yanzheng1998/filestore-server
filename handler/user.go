dd86f5b233a9794b4d8225501a16d421fb5c961b
Yanzheng Li <you@example.com>
2020-05-18 01:27:06 -0500

    add user modules

---
+++ handler/user.go
@@ -0,0 +1,86 @@
+package handler
+
+import (
+	"filestore-server/util"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	dblayer "filestore-server/db"
+	"time"
+)
+
+const (
+	pwd_salt = "*#890"
+)
+
+// SignupHandler : handler user registration
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		data, err := ioutil.ReadFile("./static/view/signup.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+		return
+	}
+	r.ParseForm()
+	username := r.Form.Get("username")
+	passwd := r.Form.Get("password")
+
+	if len(username) < 3 || len(passwd) < 5 {
+		w.Write([]byte("Invalid parameter"))
+		return
+	}
+
+	enc_passwrd := util.Sha1([]byte(passwd + pwd_salt))
+	isSuccess := dblayer.UserSignUp(username, enc_passwrd)
+	if isSuccess {
+		w.Write([]byte("SUCCESS"))
+	} else {
+		w.Write([]byte("FAILED"))
+	}
+}
+
+func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		data, err := ioutil.ReadFile("./static/view/signin.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+		return
+	}
+	r.ParseForm()
+
+	username := r.Form.Get("username")
+	password := r.Form.Get("password")
+
+	encPassword := util.Sha1([]byte(password + pwd_salt))
+
+	pwdChecked := dblayer.UserSignUp(username, encPassword)
+
+	if !pwdChecked {
+		w.Write([]byte("FAILED"))
+		return
+	}
+
+	// Generate token
+	token := GenToken(username)
+	upRes := dblayer.UpdateToken(username, token)
+	if !upRes {
+		w.Write([]byte("FAILED"))
+		return
+	}
+
+	// redirection
+	w.Write([]byte("http://" + r.Host + "/static/view/home.html"))
+}
+
+func GenToken(username string) string {
+	// 40 bit token md5(username + timestamp + token_salt) + timestamp[:8]
+	timestamp := fmt.Sprint("%x", time.Now().Unix())
+	tokenPrefix := util.MD5([]byte(username + timestamp + "_tokensalt"))
+	return tokenPrefix + timestamp[:8]
+}
